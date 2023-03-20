49966bb76f1a7f8b214e4c0ba2c5ec51fe2fa5dd
Federico De Grazia <Federico_De_Grazia@bose.com>
2023-03-20 17:05:08 -0300

    initial commit

---
+++ Solucion/AWS Servicies/RDS/DDL.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Unificado(
+	CHROM varchar(20) NULL,
+	POS varchar(20) NULL,
+	ID varchar(50) NULL,
+	REF varchar(30) NULL,
+	ALT varchar(20) NULL,
+	QUAL varchar(30) NULL,
+	FILTER varchar(10) NULL,
+	INFO varchar(700) NULL,
+	FORMA varchar(100) NULL,
+	MUESTRA varchar(20) NULL,
+	VALOR varchar(100) NULL,
+	ORIGEN varchar(900) NULL,
+	FECHA_COPIA datetime NULL,
+	RESULTADO varchar(20) NULL
+);
